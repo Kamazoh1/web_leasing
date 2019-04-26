5fde939d75b6ce04ffd2273854d35dc0a25b6a6f
Marina <marina_a_g@mail.ru>
2019-04-26 23:12:44 +0300

    login, logout, new user registration, several html drafts

--- webapp/forms.py
+++ webapp/forms.py
@@ -6,7 +6,7 @@ from wtforms import StringField, PasswordField, SubmitField, RadioField, Boolean
 from wtforms.validators import DataRequired, Email
 #validator - класс, который помогает избежать ручных проверок
 #DataRequired - проверяет, что пользователь действительно вбил данные
-
+from webapp.model import GENDER, ANN_TYPE, ROLE
 
 
 class RegistrationForm (FlaskForm): #наследуем форму от FlaskForm
@@ -14,15 +14,12 @@ class RegistrationForm (FlaskForm): #наследуем форму от FlaskFor
     surname=StringField('Фамилия пользователя', validators=[DataRequired('Введите фамилию')])
     password=PasswordField('Пароль', validators=[DataRequired('Какой вы хотите себе пароль?')])
     email=StringField('Адрес эл.почты', validators=[DataRequired('Какой у вас адрес электронной почты?'), Email('Тут нужен адрес электронной почты')])
-    gender=RadioField('Пол', choices=(
-    ('M', 'Мужской'),
-    ('F', 'Женский'),
-    ('O', 'Другой')), validators=[DataRequired('Какого вы пола?')])
+    gender=RadioField('Пол', choices=GENDER, validators=[DataRequired('Какого вы пола?')])
     submit=SubmitField('Зарегистрироваться')
 
 
 class LoginForm (FlaskForm):
     email=StringField('Ваш адрес электронной почты:', validators=[DataRequired('Укажите Ваш адрес эл.почты')])
     password=PasswordField('Пароль:', validators=[DataRequired('Введите Ваш пароль')])
-    remember=BooleanField('Запомнить', default=True, render_kw={"class":"form_check_input"})
+    remember=BooleanField('Запомнить', default=False, render_kw={"class":"form_check_input"})
     submit=SubmitField('Зайти')
